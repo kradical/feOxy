0f315f5e44ece54bd90f3c76836e84605dcdac7e
krad <konrad_here@hotmail.com>
2016-08-13 00:06:45 -0700

    super initial render code

---
+++ src/render.rs
@@ -0,0 +1,24 @@
+use layout::{LayoutBox, Rect};
+use css::{Color};
+
+type DisplayList = Vec<DisplayCommand>;
+
+struct DisplayCommand {
+    SolidRect(Color, Rect),
+}
+
+fn build_display_commands(root: &LayoutBox) -> DisplayList {
+    let mut commands = Vec::new();
+    renter_layout_box(&mut commands, root);
+    commands
+}
+
+fn render_layout_box(commands: &mut DisplayList, layout_box: &LayoutBox) {
+    render_background(commands, layout_box);
+    render_borders(commands, layout_box);
+    //TODO: Render text
+
+    for child in &layout_box.children {
+        render_layout_box(commands, child);
+    }
+}
